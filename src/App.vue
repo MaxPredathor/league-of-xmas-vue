3b0e7a5585a1b325b84e3aa579558a1d1588fa56
MaxPredathor <max.gilli98@gmail.com>
2023-12-28 20:36:18 +0100

    few merge related changes

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,23 @@
   <div class="container">
     <router-view></router-view>
   </div>
+  <footer>
+    <FooterComponent />
+  </footer>
 </template>
 
 <script>
 import Home from "./views/Home.vue";
 import Champion from "./views/Champions.vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 export default {
   name: "App",
   components: {
     Home,
     Champion,
     HeaderComponent,
+    FooterComponent,
   },
 };
 </script>
@@ -25,6 +30,11 @@ export default {
 @use "./assets/style/partials/variables" as *;
 
 header {
+  background-color: $color-prussian-blue;
+  color: $color-sky-blue;
+}
+
+footer {
   background-color: $color-prussian-blue;
   color: $color-sky-blue;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,17 @@
 <template>
-  <header>
-    <HeaderComponent />
-  </header>
-  <div class="container">
-    <router-view></router-view>
+  <div>
+    <h1>This is the Home page!</h1>
   </div>
 </template>
 
 <script>
-import Home from "./views/Home.vue";
-import Champion from "./views/Champions.vue";
-import HeaderComponent from "./components/HeaderComponent.vue";
 export default {
-  name: "App",
+  name: "HomeRoute",
   components: {
-    Home,
-    Champion,
-    HeaderComponent,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@use "./assets/style/partials/variables" as *;
-
-header {
-  background-color: $color-prussian-blue;
-  color: $color-sky-blue;
-}
+@use "../assets/style/partials/variables" as *;
 </style>
