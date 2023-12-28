4171ca4f88618b0f5d3cf9200e15b24c0d3036a2
MaxPredathor <max.gilli98@gmail.com>
2023-12-28 09:25:36 +0100

    added getChamp function with axios call

--- src/views/Champions.vue
+++ src/views/Champions.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <h1>This is the Champions page</h1>
-  </div>
+    <div>
+        <h1>This is the Champions page</h1>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Champions',
     data() {
@@ -13,10 +14,27 @@ export default {
         }
     },
     methods: {
-
-    }
+        getChamps() {
+            const apiUrl = 'https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json';
+            axios
+                .get(apiUrl)
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.champs = response.data.data
+                    this.champions = response.data.data
+                    console.log(this.champions)
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    },
     mounted() {
-
+        this.getChamps()
     },
 }
 </script>
