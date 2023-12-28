6ed0c7eb5ff4001744ddf614bece90bd2fb4adf5
IIODERII <oderrisi@gmail.com>
2023-12-28 09:31:15 +0100

    added reactive url for api in chamions.vue

--- src/views/Champions.vue
+++ src/views/Champions.vue
@@ -1,42 +1,43 @@
 <template>
-    <div>
-        <h1>This is the Champions page</h1>
-    </div>
+  <div>
+    <h1>This is the Champions page</h1>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import { store } from "../assets/data/store.js";
 export default {
-    name: 'Champions',
-    data() {
-        return {
-            champions: []
-        }
+  name: "Champions",
+  data() {
+    return {
+      champions: [],
+    };
+  },
+  methods: {
+    getChamps() {
+      const apiUrl = store.ChampionsUrls.allChamps;
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          console.log(response.data.data);
+          this.champs = response.data.data;
+          this.champions = response.data.data;
+          console.log(this.champions);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+        });
     },
-    methods: {
-        getChamps() {
-            const apiUrl = 'https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json';
-            axios
-                .get(apiUrl)
-                .then((response) => {
-                    console.log(response.data.data);
-                    this.champs = response.data.data
-                    this.champions = response.data.data
-                    console.log(this.champions)
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-                .finally(function () {
-                    // always executed
-                });
-        }
-    },
-    mounted() {
-        this.getChamps()
-    },
-}
+  },
+  mounted() {
+    this.getChamps();
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
