5e162b482d01c37a5684f29318744c8bcde245e3
IIODERII <oderrisi@gmail.com>
2023-12-28 10:13:27 +0100

    added filtered champions

--- src/partials/ChampionCard.vue
+++ src/partials/ChampionCard.vue
@@ -1,31 +1,35 @@
 <template>
+  <div class="col-4" v-if="champ.tags.includes(activeTag) || activeTag == ''">
     <div class="card-div">
-        <img :src="store.ChampionsUrls.champImage + champ.id + '_0.jpg'" :alt="champ.name">
-        <h3>{{ champ.name }}</h3>
+      <img
+        :src="store.ChampionsUrls.champImage + champ.id + '_0.jpg'"
+        :alt="champ.name"
+      />
+      <h3>{{ champ.name }}</h3>
     </div>
+  </div>
 </template>
 
 <script>
 import { store } from "../assets/data/store.js";
 export default {
-    name: "ChampionCard",
-    props: {
-        champ: Object
-    },
-    data() {
-        return {
-            store,
-
-        }
-    }
-}
+  name: "ChampionCard",
+  props: {
+    champ: Object,
+    activeTag: String,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
-    
+
 <style lang="scss" scoped>
 .card-div {
-
-    img {
-        width: 100%;
-    }
+  img {
+    width: 100%;
+  }
 }
 </style>
--- src/views/Champions.vue
+++ src/views/Champions.vue
@@ -1,12 +1,58 @@
 <template>
-    <div>
-        <h1>This is the Champions page</h1>
-        <div class="row">
-            <div class="col-4" v-for="champ in champions" :key="champ.key">
-                <ChampionsCard :champ="champ" />
-            </div>
+  <div>
+    <h1>This is the Champions page</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <button
+                class="nav-link active"
+                aria-current="page"
+                @click="tag = ''"
+              >
+                All
+              </button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" @click="tag = 'Assassin'">
+                Assassins
+              </button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" @click="tag = 'Fighter'">
+                Fighters
+              </button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" @click="tag = 'Mage'">Mages</button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" @click="tag = 'Marksman'">
+                Marksmen
+              </button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" @click="tag = 'Support'">
+                Supports
+              </button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" @click="tag = 'Tank'">Tanks</button>
+            </li>
+          </ul>
         </div>
+      </div>
+    </nav>
+    <div class="row">
+      <ChampionsCard
+        v-for="champ in champions"
+        :key="champ.key"
+        :champ="champ"
+        :activeTag="tag"
+      />
     </div>
+  </div>
 </template>
 
 <script>
@@ -14,38 +60,39 @@ import axios from "axios";
 import { store } from "../assets/data/store.js";
 import ChampionsCard from "../partials/ChampionCard.vue";
 export default {
-    name: "Champions",
-    components: {
-        ChampionsCard,
+  name: "Champions",
+  components: {
+    ChampionsCard,
+  },
+  data() {
+    return {
+      champions: [],
+      tag: "",
+    };
+  },
+  methods: {
+    getChamps() {
+      const apiUrl = store.ChampionsUrls.allChamps;
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          console.log(response.data.data);
+          this.champs = response.data.data;
+          this.champions = response.data.data;
+          console.log(this.champions);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+        });
     },
-    data() {
-        return {
-            champions: [],
-        };
-    },
-    methods: {
-        getChamps() {
-            const apiUrl = store.ChampionsUrls.allChamps;
-            axios
-                .get(apiUrl)
-                .then((response) => {
-                    console.log(response.data.data);
-                    this.champs = response.data.data;
-                    this.champions = response.data.data;
-                    console.log(this.champions);
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-                .finally(function () {
-                    // always executed
-                });
-        },
-    },
-    mounted() {
-        this.getChamps();
-    },
+  },
+  mounted() {
+    this.getChamps();
+  },
 };
 </script>
 
