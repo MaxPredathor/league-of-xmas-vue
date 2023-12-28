f860d3aa9563b2b103e9b21384caf7c9833992bf
MaxPredathor <max.gilli98@gmail.com>
2023-12-28 10:55:57 +0100

    added some hover effects

--- src/partials/ChampionCard.vue
+++ src/partials/ChampionCard.vue
@@ -1,63 +1,77 @@
 <template>
-  <div class="col-4" v-if="champ.tags.includes(activeTag) || activeTag == ''">
-    <div class="card-div">
-      <div>
-        <img
-          :src="store.ChampionsUrls.champImage + champ.id + '_' + skin + '.jpg'"
-          :alt="champ.name"
-        />
-      </div>
-      <h3>
-        {{ champ.name }} - <em class="fw-regular">{{ champ.title }}</em>
-      </h3>
+    <div class="col-4" v-if="champ.tags.includes(activeTag) || activeTag == ''">
+        <div class="card-div">
+            <div>
+                <img :src="store.ChampionsUrls.champImage + champ.id + '_' + skin + '.jpg'" :alt="champ.name" />
+            </div>
+            <h3>
+                <span>{{ champ.name }} - <em class="fw-regular">{{ champ.title }}</em></span>
+            </h3>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import { store } from "../assets/data/store.js";
 export default {
-  name: "ChampionCard",
-  props: {
-    champ: Object,
-    activeTag: String,
-  },
-  data() {
-    return {
-      store,
-      skin: 0,
-    };
-  },
+    name: "ChampionCard",
+    props: {
+        champ: Object,
+        activeTag: String,
+    },
+    data() {
+        return {
+            store,
+            skin: 0,
+        };
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .card-div {
-  position: relative;
-  margin-bottom: 20px;
-  div {
-    overflow: hidden;
+    position: relative;
+    margin-bottom: 20px;
 
-    img {
-      width: 100%;
-      transition: all 0.1s linear;
+    div {
+        overflow: hidden;
+
+        img {
+            width: 100%;
+            transition: all 0.1s linear;
+        }
     }
-  }
-  h3 {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    padding: 10px 15px;
-    background-color: rgba($color: #000000, $alpha: 0.5);
-    color: #fff;
-    margin: 0;
-    width: 100%;
-    font-size: 1.3em;
-  }
 
-  &:hover img {
-    transform: scale(1.1);
-    transition: all 0.3s linear;
-  }
+    h3 {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        padding: 10px 15px;
+        background-color: rgba($color: #000000, $alpha: 0.5);
+        color: #fff;
+        margin: 0;
+        width: 100%;
+        font-size: 1.3em;
+        transition: all 0.1s linear;
+    }
+
+    span {
+        transition: all 0.1s linear;
+    }
+
+    &:hover img {
+        transform: scale(1.1);
+        transition: all 0.3s linear;
+    }
+
+    &:hover span {
+        margin-left: 10px;
+        transition: all 0.2s linear;
+    }
+
+    &:hover h3 {
+        background-color: rgba($color: #000000, $alpha: 1);
+        transition: all 0.2s linear;
+    }
 }
 </style>
