3f1cd3612724ae73c74ee4ced25df5990123c79e
iaio998 <depalatisiacopo@gmail.com>
2023-12-29 11:45:22 +0100

    Added folder homepgae

---
+++ src/components/homepage/SectionChampions.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <p class="m-0">Here will be wrote something</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <h1>This is the Home page!</h1>
+
+    <h2>This is the section where swiper will show champions</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeRoute",
-  components: {
-  },
+  components: {},
 };
 </script>
 
