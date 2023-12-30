87265895c92da46ea4cceebb88564a4cc1ad3c06
MaxPredathor <max.gilli98@gmail.com>
2023-12-30 15:33:26 +0100

    improved skin selector also added scrollBy function on click

--- src/components/show-page/Skins.vue
+++ src/components/show-page/Skins.vue
@@ -4,15 +4,15 @@
             <div class="row">
                 <div class="col-12"
                     :style="{ 'background-image': 'url(' + store.ChampionsUrls.champImage + store.activeChamp + '_' + skinDisplay + '.jpg' + ')' }">
-                    <div class="available">
+                    <div class="available" ref="skins">
                         <h1> Aveilable Skins</h1>
                         <div class="skins" v-for="item in obj">
-                            <div class="skin-container" v-for="skin in item.skins" @click="skinDisplay = skin.num">
+                            <div class="skin-container" v-for="skin in item.skins" @click="scrollSkin(skin.num)">
                                 <div class="skin-img">
                                     <img :src="store.ChampionsUrls.champImage + store.activeChamp + '_' + skin.num + '.jpg'"
                                         alt="">
                                 </div>
-                                <div v-if="skin.num !== 0" class="skin" @click="skinDisplay = skin.num">
+                                <div v-if="skin.num !== 0" class="skin">
                                     {{ skin.name.toUpperCase() }}
                                 </div>
                                 <div v-else class="skin">
@@ -35,10 +35,31 @@ export default {
         return {
             store,
             skinDisplay: 0,
+            lastSkin: 0,
         }
     },
     props: {
         obj: Object,
+    },
+    methods: {
+        scrollSkin(num) {
+            this.skinDisplay = num
+            const mySkin = this.$refs.skins
+            if (this.lastSkin > num) {
+                mySkin.scrollBy({
+                    top: -80,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            } else {
+                mySkin.scrollBy({
+                    top: 80,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            }
+            this.lastSkin = num;
+        }
     }
 }
 </script>
@@ -56,15 +77,18 @@ export default {
                 margin: 40px 0;
                 height: 850px;
                 padding: 0;
-                border: 1px solid black;
                 background-repeat: no-repeat;
                 background-size: cover;
                 transition: all 0.4s linear;
 
                 .available {
                     height: 100%;
-                    width: 25%;
-                    background-color: rgba(0, 0, 0, 0.8);
+                    width: 35%;
+                    background-color: rgba(0, 0, 0, 0.5);
+                    backdrop-filter: blur(6px);
+                    display: flex;
+                    flex-direction: column;
+                    flex-wrap: nowrap;
                     overflow: hidden;
 
                     h1 {
@@ -75,26 +99,35 @@ export default {
                         font-weight: bolder;
                         color: white;
                         margin: 0;
-                        border-bottom: 1px solid white;
                     }
 
                     .skins {
+                        user-select: none;
+                        scrollbar-width: none;
+
+                        &::-webkit-scrollbar {
+                            display: none;
+                        }
 
                         .skin-container {
                             display: flex;
-                            margin: 10px 0;
+                            margin: 20px 0;
                             cursor: pointer;
 
                             .skin {
+                                width: 85%;
                                 font-family: $font-spiegel;
-                                font-size: 1em;
+                                font-size: 0.8em;
                                 color: white;
                                 height: 50px;
-                                padding: 5px 10px;
+                                padding: 5px 0;
+                                display: flex;
+                                align-items: center;
+                                font-weight: bold;
                             }
 
                             .skin-img {
-                                width: 30%;
+                                width: 15%;
                                 margin-left: 20px;
 
                                 img {
