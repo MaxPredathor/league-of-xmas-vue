89f0ead7b3fe710de42fa274d581ed8b8df5a018
MaxPredathor <max.gilli98@gmail.com>
2023-12-31 11:46:08 +0100

    wip button

--- src/components/show-page/VideoLol.vue
+++ src/components/show-page/VideoLol.vue
@@ -3,6 +3,7 @@
         <video autoplay loop muted ref="video">
             <source :src="currentVideo" type="video/mp4">
         </video>
+        <button>GIOCA GRATIS</button>
     </div>
 </template>
 
@@ -47,9 +48,68 @@ export default {
 div {
     width: 100%;
     background-color: black;
+    position: relative;
 
     video {
         width: 100%;
+    }
+
+    button {
+        width: 200px;
+        height: 60px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #13d8f6;
+        font-size: 0.7em;
+        letter-spacing: 0.15em;
+        font-weight: bold;
+        color: black;
+        border: none;
+
+
+        &::before {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 210px;
+            height: 70px;
+            display: block;
+            clip-path: polygon(15% 0, 100% 0, 100% 70%, 85% 100%, 0 100%, 0 30%);
+            content: "";
+            border-top: 1px solid #8595a5;
+            border-right: 1px solid #b1beca;
+            border-left: 1px solid #b1beca;
+            border-bottom: 1px solid #8595a5;
+            // border-radius: 19% 28% 13% 28% / 35% 0% 24% 0%;
+
+        }
+
+        // position: relative;
+        // display: inline-block;
+        // padding: 15px 30px;
+        // background: linear-gradient(45deg, #3498db, #3498db 50%, #2980b9 50%, #2980b9);
+        // color: #fff;
+        // text-decoration: none;
+        // font-size: 16px;
+        // transition: all 0.3s ease;
+
+
+        // &::before {
+        //     content: '';
+        //     position: absolute;
+        //     top: 0;
+        //     left: 0;
+        //     width: 100%;
+        //     height: 100%;
+        //     border: 2px solid #2980b9;
+        //     box-sizing: border-box;
+        //     transform-origin: top left;
+        //     transform: scaleY(0);
+        //     transition: transform 0.3s ease;
+        // }
     }
 }
 </style>
