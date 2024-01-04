73a3de712aa42a9c85f5b812023c17051e50da5f
iaio998 <depalatisiacopo@gmail.com>
2024-01-04 11:22:16 +0100

    Fixed jumbo section

--- src/components/homepage/SectionJumbotron.vue
+++ src/components/homepage/SectionJumbotron.vue
@@ -10,11 +10,19 @@
     class="mySwiper"
   >
     <swiper-slide class="position-relative" v-for="item in imgs">
-      <img :src="imgUrl + item.img" />
-      <div class="position-absolute title-p text-center">
-        <h1 class="display-4 px-1 m-0 w-75 m-auto">{{ item.title }}</h1>
-        <p class="fs-3 m-0 w-50 m-auto">{{ item.description }}</p>
+      <div v-if="item != 'video'">
+        <img :src="imgUrl + item.img" />
+        <div class="title-p text-center">
+          <h1 class="display-4 px-1">{{ item.title }}</h1>
+          <p class="fs-3">{{ item.description }}</p>
+        </div>
       </div>
+      <video autoplay loop v-else>
+        <source
+          type="video/webm"
+          src="https://static.bigbrain.gg/assets/ugg/pages/desktop-app/ugg-app-landing.webm"
+        />
+      </video>
     </swiper-slide>
   </swiper>
 </template>
@@ -46,18 +54,19 @@ export default {
         {
           title: "Welcome to the League of Xmas!",
           description: "Enjoy your time visiting our website",
-          img: "hwei-lol-splash-art-4k-wallpaper-uhdpaper.com-41@1@n.jpg",
-        },
-        {
-          title: "Play with your friends",
-          description: "Register to play and have fun with your friends",
-          img: "lol-heartsteel-ksante-ezreal-yone-aphelios-sett-kayn-4k-wallpaper-uhdpaper.com-42@1@n.jpg",
+          img: "leagueofxmas.jpg",
         },
         {
           title: "Visit champions",
-          description: "Visit to see all champions stats and more",
-          img: "heartsteel-yone-prestige-edition-skin-lol-splash-art-2k-wallpaper-uhdpaper.com-664@1@m.jpg",
+          description: "Check out all the champions",
+          img: "allchamps.jpg",
         },
+        {
+          title: "See champions stats",
+          description: "Visit to see all champions stats and more",
+          img: "specificchamp.jpg",
+        },
+        "video",
       ],
     };
   },
@@ -81,15 +90,19 @@ export default {
   width: 100%;
   height: 100%;
   object-fit: cover;
-  object-position: top;
 }
 
 .title-p {
-  max-width: 50%;
-  max-height: 100%;
+  width: 500px;
+  max-height: 250px;
   background-color: rgba(0, 0, 0, 0.288);
   bottom: 50%;
   left: 50%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   transform: translate(-50%, 50%);
   color: goldenrod;
   font-family: $font-LOL;
@@ -98,6 +111,10 @@ export default {
   -webkit-text-stroke-width: 2px;
   -webkit-text-stroke-color: white;
   font-style: italic;
+  h1,
+  p {
+    width: 90%;
+  }
   p {
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: white;
