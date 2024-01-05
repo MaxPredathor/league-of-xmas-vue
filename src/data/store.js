a0742ae66807a65d7a85180f822df0e954e6e692
iaio998 <depalatisiacopo@gmail.com>
2024-01-05 17:39:58 +0100

    Added active user

--- src/data/store.js
+++ src/data/store.js
@@ -93,6 +93,9 @@ export const store = reactive({
     "https://cdn.lolrift.com/images/rrunes/runes/" /* + nome runa con l'iniziale in maiuscolo (se ha più parole si mette un _ in mezzo e tutte le parole con iniziale maiuscola) + _rune.png */,
   runeImage2:
     "https://ddragon.canisback.com/img/perk-images/Styles/" /* + idStrano_Name.png */,
+
+  activeUser: "",
+  activeServer: "",
 });
 
 //https://static.developer.riotgames.com/docs/lol/queues.json
