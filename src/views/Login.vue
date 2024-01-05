bcf4321fe171d4263f05b3c735d46f1b66d1e4d9
MaxPredathor <max.gilli98@gmail.com>
2024-01-05 14:32:42 +0100

    added several feature to login/register pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,54 @@
 <template>
-  <h1>Login</h1>
-  <div>
-    <form class="mx-2">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        />
-      </div>
-      <button type="submit" class="btn btn-primary my-2">Submit</button>
-    </form>
+  <div class="container">
+    <h1>Login</h1>
+    <div class="row">
+      <form class="mx-2">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="form-group position-relative">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            :type="returnInputType()"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+          <span @click="showPassword = !showPassword">
+            <i v-if="!showPassword" class="fa-regular fa-eye eye"></i>
+            <i v-else class="fa-regular fa-eye-slash eye"></i>
+          </span>
+        </div>
+        <button type="submit" class="btn btn-primary my-2">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Login",
+  data() {
+    return {
+      showPassword: false,
+    };
+  },
+  methods: {
+    returnInputType() {
+      if (this.showPassword) {
+        return "text";
+      } else {
+        return "password";
+      }
+    },
+  },
   beforeCreate() {
     this.$root.showHeader = false;
     this.$root.showFooter = false;
@@ -36,4 +56,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.eye {
+  cursor: pointer;
+  position: absolute;
+  right: 1%;
+  top: 55%;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,51 +1,71 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Register</h1>
-    <form class="mx-2">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Summoner Name</label>
-        <input
-          type="text"
-          class="form-control"
-          id="exampleInputSummonerName1"
-          placeholder="Your Summoner Name"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Server</label> <br />
-        <select name="server" id="server">
-          <option value="#EUW">Europe (EUW)</option>
-          <option value="#NA">North America (NA)</option>
-          <option value="#KR">South Korea (KR)</option>
-        </select>
-      </div>
-      <button type="submit" class="btn btn-primary my-2">Submit</button>
-    </form>
+    <div class="row">
+      <form class="mx-2">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="form-group position-relative">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            :type="returnInputType()"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+          <span @click="showPassword = !showPassword">
+            <i v-if="!showPassword" class="fa-regular fa-eye eye"></i>
+            <i v-else class="fa-regular fa-eye-slash eye"></i>
+          </span>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Summoner Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputSummonerName1"
+            placeholder="Your Summoner Name"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Server</label> <br />
+          <select name="server" id="server">
+            <option value="#EUW">Europe (EUW)</option>
+            <option value="#NA">North America (NA)</option>
+            <option value="#KR">South Korea (KR)</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary my-2">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Register",
+  data() {
+    return {
+      showPassword: false,
+    };
+  },
+  methods: {
+    returnInputType() {
+      if (this.showPassword) {
+        return "text";
+      } else {
+        return "password";
+      }
+    },
+  },
   beforeCreate() {
     this.$root.showHeader = false;
     this.$root.showFooter = false;
@@ -53,4 +73,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.eye {
+  cursor: pointer;
+  position: absolute;
+  right: 1%;
+  top: 55%;
+}
+</style>
